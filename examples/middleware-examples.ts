cb065a1846322269eed59d52a12b31a09952fd5e
Josh <joshhendo@outlook.com>
2017-07-29 12:06:04 +1000

    Fixed up middleware handling and added in example

---
+++ examples/middleware-examples.ts
@@ -0,0 +1,25 @@
+import * as express from 'express';
+import {createApplication, HttpResponse} from '../index';
+import * as bodyParser from 'body-parser';
+
+const app = createApplication();
+const jsonParser = bodyParser.json();
+
+app.get('/', function (req: express.Request, res: express.Response) {
+  // You can, and should, still set things via accessing `res`, such as content type and headers
+  // Currently, returning can only set the body and code
+  res.type('text/html');
+
+  return new HttpResponse(`Try POSTing some json to '/post-something'.`)
+});
+
+app.post('/post-something', jsonParser, function (req) {
+  if (!req.body || JSON.stringify(req.body) == '{}') {
+    return new HttpResponse(`You POSTed invalid or empty JSON!`);
+  }
+  return new HttpResponse(`You posted up: ${JSON.stringify(req.body)}`);
+});
+
+app.listen(3002, function () {
+  console.log('Example app listening on port 3002');
+});
--- lib/models/http-response.ts
+++ lib/models/http-response.ts
@@ -1,10 +1,9 @@
-
 export class HttpResponse {
-    body: any;
-    code: number;
+  body: any;
+  code: number;
 
-    constructor(body: any, code?: number) {
-        this.body = body;
-        this.code = code;
-    }
+  constructor(body: any, code?: number) {
+    this.body = body;
+    this.code = code;
+  }
 }
